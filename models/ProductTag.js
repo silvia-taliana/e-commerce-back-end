abecb094304ab129d6dc101307a5291ab71928ba
Silvia Taliana <silviataliana2@gmail.com>
2021-03-11 15:32:09 +1030

    defining columns and foreign keys in models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,33 @@
 const { Model, DataTypes } = require('sequelize');
+const { databaseVersion } = require('../../../01-Activities/26-Stu_Literals/Solved/config/connection');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
