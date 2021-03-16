d94c5a60c751f43c35133cb1bc595817e382ffb5
Silvia Taliana <silviataliana2@gmail.com>
2021-03-16 12:44:25 +1030

    adding function to create new tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -29,8 +29,17 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create({
+      id: req.body.id,
+      tag_name: req.body.tag_name,
+    });
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
