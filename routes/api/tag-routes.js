830bef9755b6666edaa28a831282eac7b124128b
Silvia Taliana <silviataliana2@gmail.com>
2021-03-16 12:53:11 +1030

    created function to delete tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -42,12 +42,43 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update(req.body, {
+      tag_name: req.body.tag_name,
+      where: {
+        id: req.params.id,
+      }
+    });
+    res.status(200).json(updateTag);
+    if (!updateTag) {
+      res.status(404).json({ message: `Wrong ID` })
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleteTag = await Tag.destroy({
+      tag_name: req.body.tag_name,
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deleteTag) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(deleteTag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
