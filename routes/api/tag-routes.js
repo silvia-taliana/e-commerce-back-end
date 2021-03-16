1bcbdb40f394750f7a53be84223106a48c633a8f
Silvia Taliana <silviataliana2@gmail.com>
2021-03-16 12:31:30 +1030

    adding function to get tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -16,9 +16,17 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagId = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tagId);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
